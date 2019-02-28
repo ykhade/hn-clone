1af62ba8fcf535f1f85efa1cbf05fdb609c40ce6
yashkhade <ykhade@nevada.unr.edu>
2019-02-27 20:21:43 -0800

    Added the colors and global stylings

--- src/styles/palette.js
+++ src/styles/palette.js
@@ -1,2 +1,18 @@
 //Adding a dark and light colors
 
+export const colorsDark = {
+    background:'#272727',
+    backgroundSecondary:'#393C3E',
+    text:'#bfbebe',
+    textSecondary:'#848886',
+    border:'#272727',
+
+};
+
+export const colorsLight = {
+    background: '#EAEAEA',
+    backgroundSecondary: '#F8F8F8',
+    text:'#848886',
+    textSecondary: '#aaaaaa',
+    border: 'EAEAEA',
+};
