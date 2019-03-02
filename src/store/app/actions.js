80892f1a60cd6b5dcc52c44040bcdc12d73ec587
yashkhade <ykhade@nevada.unr.edu>
2019-03-01 22:23:13 -0800

    Added all the middleware

--- src/store/app/actions.js
+++ src/store/app/actions.js
@@ -1,11 +1,15 @@
-const NS = '@hnClone/App';
+import { buildActionCreator } from 'store/utils';
+
+const NS = '@hackerNewsReader/app';
 
 export const actionTypes = {
     SET_THEME: `${NS}/SET_THEME`,
+    SET_LAYOUT: `${NS}/SET_LAYOUT`,
 };
 
 const actions = {
-    setTheme: (payload = {}) => ({ type: actionTypes.SET_THEME, payload}),
+    setTheme: buildActionCreator(actionTypes.SET_THEME),
+    setLayout: buildActionCreator(actionTypes.SET_LAYOUT),
 };
 
 export default actions;
