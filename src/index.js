8d812a22bb27c084af2ba082b51805be5f21e71f
yashkhade <ykhade@nevada.unr.edu>
2019-03-01 22:32:31 -0800

    fixed index.js and serviceworker

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'components/App';
-import GlobalStyles from 'styles/globals'
-import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import configureStore from 'store';
-
-
+import App from 'components/App';
+import GlobalStyles from 'styles/globals';
+import loadInitialState from 'store/middleware/localStorageMiddleware/loadInitialState';
+import registerServiceWorker from './registerServiceWorker';
 
 const renderApp = () => {
-    const store = configureStore({});
+    const initialState = loadInitialState();
+    const store = configureStore(initialState);
+
     if (process.env.NODE_ENV !== 'production') {
-        console.log('Initial State ->');
+        console.log('Initial state ->');
         console.log(store.getState());
     }
 
     ReactDOM.render(
         <Provider store={store}>
-        <div>
-            <GlobalStyles/>
-            <App/>
-        </div>
+            <div>
+                <GlobalStyles />
+                <App />
+            </div>
         </Provider>,
         document.getElementById('root'),
     );
@@ -28,7 +29,4 @@ const renderApp = () => {
 
 renderApp();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+registerServiceWorker();
