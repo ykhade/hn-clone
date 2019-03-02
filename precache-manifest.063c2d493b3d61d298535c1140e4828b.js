f99ecf76cb699d57271ee6cac6f14083a0cf3105
yashkhade <ykhade@nevada.unr.edu>
2019-03-01 23:44:38 -0800

    Updates

---
+++ precache-manifest.063c2d493b3d61d298535c1140e4828b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac67ea63cb3c06256e37",
+    "url": "/hn-clone/static/js/runtime~main.ac67ea63.js"
+  },
+  {
+    "revision": "0fdaaabc5da410066298",
+    "url": "/hn-clone/static/js/main.0fdaaabc.chunk.js"
+  },
+  {
+    "revision": "43e657af931fd174ee0c",
+    "url": "/hn-clone/static/js/2.43e657af.chunk.js"
+  },
+  {
+    "revision": "6d509480b49c408e9130629a03929224",
+    "url": "/hn-clone/index.html"
+  }
+];
